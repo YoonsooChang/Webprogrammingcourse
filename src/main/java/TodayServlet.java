828f6333ddde7e9aa11b5978ded732f81d014490
ysc9606 <ysc9606@nts-corp.com>
2021-03-31 17:53:22 +0900

    naver-eclipse-formatter 적용 HTML/CSS 컨벤션 적용

--- src/main/java/TodayServlet.java
+++ src/main/java/TodayServlet.java
@@ -12,25 +12,27 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/today")
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public TodayServlet() {
-        super();
-    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public TodayServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
 
 		PrintWriter htmlContentsWriter = response.getWriter();
 		String currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/M/d HH:mm"));
-		
+
 		htmlContentsWriter.println("<html>");
 		htmlContentsWriter.println("<head><title>My Homepage</title></head>");
 		htmlContentsWriter.println("<body>");
 		htmlContentsWriter.println("<a href='/aboutme/index.html'>메인화면</a>");
-		htmlContentsWriter.println("<h1 style=\"text-align:center;line-height:50vh;margin:20vh 0\">현재 시간 : " + currentTime + "</h1>");
+		htmlContentsWriter
+			.println("<h1 style=\"text-align:center;line-height:50vh;margin:20vh 0\">현재 시간 : " + currentTime + "</h1>");
 		htmlContentsWriter.println("</body>");
 		htmlContentsWriter.println("</html>");
-		
+
 		htmlContentsWriter.close();
 	}
 
