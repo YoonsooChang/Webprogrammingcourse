5115813c9a703f97b031f809df53851cd3bd9167
ysc9606 <ysc9606@nts-corp.com>
2021-05-07 01:33:37 +0900

    FIXED/API/CommentMapper 리팩토링

--- src/main/java/kr/or/connect/reservation/mapper/CommentMapper.java
+++ src/main/java/kr/or/connect/reservation/mapper/CommentMapper.java
@@ -12,52 +12,59 @@ import kr.or.connect.reservation.dto.Comment;
 import kr.or.connect.reservation.dto.CommentImage;
 
 public class CommentMapper implements ResultSetExtractor<List<Comment>> {
+	private final int NONE_ASSIGNED = 0;
+
+	private Comment extractComment(ResultSet rs) throws SQLException {
+		return new Comment(
+			rs.getString("comment"),
+			rs.getInt("comment_id"),
+			rs.getDate("create_date").toLocalDate(),
+			rs.getDate("modify_date").toLocalDate(),
+			rs.getInt("product_id"),
+			rs.getDate("reservation_date").toLocalDate(),
+			rs.getString("reservation_email"),
+			rs.getInt("reservation_info_id"),
+			rs.getString("reservation_name"),
+			rs.getString("reservation_telephone"),
+			rs.getDouble("score"));
+	}
+
+	private CommentImage extractCommentImage(ResultSet rs) throws SQLException {
+		return new CommentImage(
+			rs.getString("content_type"),
+			rs.getDate("file_create_date").toLocalDate(),
+			rs.getBoolean("delete_flag"),
+			rs.getInt("file_id"),
+			rs.getString("file_name"),
+			rs.getInt("image_id"),
+			rs.getDate("file_modify_date").toLocalDate(),
+			rs.getInt("image_reservation_info_id"),
+			rs.getInt("reservation_comment_id"),
+			rs.getString("save_file_name"));
+	}
+
 	@Override
 	public List<Comment> extractData(ResultSet rs) throws SQLException, DataAccessException {
 		List<Comment> commentList = new ArrayList<Comment>();
-		int commentId = 0;
-		Comment comment = null;
+
+		int currentCommentId = NONE_ASSIGNED;
+		Comment currentCommentInstance = null;
 
 		while (rs.next()) {
-			if (rs.getInt("comment_id") != commentId) {
-				if (!rs.isFirst()) {
-					commentList.add(comment);
+			if (rs.getInt("comment_id") != currentCommentId) {
+				if (currentCommentInstance != null) {
+					commentList.add(currentCommentInstance);
 				}
-
-				comment = new Comment(
-					rs.getString("comment"),
-					rs.getInt("comment_id"),
-					rs.getDate("create_date").toLocalDate(),
-					rs.getDate("modify_date").toLocalDate(),
-					rs.getInt("product_id"),
-					rs.getDate("reservation_date").toLocalDate(),
-					rs.getString("reservation_email"),
-					rs.getInt("reservation_info_id"),
-					rs.getString("reservation_name"),
-					rs.getString("reservation_telephone"),
-					rs.getDouble("score"));
+				currentCommentInstance = extractComment(rs);
 			}
 
 			int imageId = rs.getInt("image_id");
 			if (imageId != 0) {
-				comment.getCommentImages().add(
-					new CommentImage(
-						rs.getString("content_type"),
-						rs.getDate("file_create_date").toLocalDate(),
-						rs.getBoolean("delete_flag"),
-						rs.getInt("file_id"),
-						rs.getString("file_name"),
-						imageId,
-						rs.getDate("file_modify_date").toLocalDate(),
-						rs.getInt("image_reservation_info_id"),
-						rs.getInt("reservation_comment_id"),
-						rs.getString("save_file_name"))
-
-				);
+				currentCommentInstance.getCommentImages().add(extractCommentImage(rs));
 			}
 
 			if (rs.isLast()) {
-				commentList.add(comment);
+				commentList.add(currentCommentInstance);
 			}
 		}
 		return commentList;
